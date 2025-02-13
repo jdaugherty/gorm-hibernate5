91a7dff657420b820bb4009e90f63ac6ce3910b2
g.sartori <g.sartori@gmail.com>
2025-02-13 14:09:11 +0100

    BootStrap init closure servletContext cleanup

--- examples/grails-hibernate/grails-app/init/functional/tests/BootStrap.groovy
+++ examples/grails-hibernate/grails-app/init/functional/tests/BootStrap.groovy
@@ -1,17 +1,20 @@
 package functional.tests
 
+import jakarta.servlet.ServletContext
 import org.grails.orm.hibernate.HibernateDatastore
 
 class BootStrap {
 
+    ServletContext servletContext
     HibernateDatastore hibernateDatastore
 
-    def init = { servletContext ->
+    def init = {
         assert hibernateDatastore.connectionSources.defaultConnectionSource.settings.hibernate.getConfigClass() == CustomHibernateMappingContextConfiguration
         Product.withTransaction {
             new Product(name: "MacBook", price: "1200.01").save()
         }
     }
+
     def destroy = {
     }
 }
--- examples/issue450/grails-app/init/multitenantcomposite/BootStrap.groovy
+++ examples/issue450/grails-app/init/multitenantcomposite/BootStrap.groovy
@@ -2,13 +2,15 @@ package multitenantcomposite
 
 import grails.gorm.multitenancy.Tenants
 import groovy.transform.CompileStatic
+import jakarta.servlet.ServletContext
 
 @CompileStatic
 class BootStrap {
 
+    ServletContext servletContext
     BookService bookService
 
-    def init = { servletContext ->
+    def init = {
         String grailsId = UUID.randomUUID().toString()
         Tenants.withId("grails") {
             bookService.save(grailsId, "The definitive Guide to Grails 2")
